ecd908ebdbb2f136b28ec67a9b3ab4741b495b52
HarveyBong <felix.nagy@chasacademy.se>
2024-04-05 13:13:34 +0200

    netlify test

--- u06-Angular/u06-frontend/src/app/pipes/recipebyid.pipe.ts
+++ u06-Angular/u06-frontend/src/app/pipes/recipebyid.pipe.ts
@@ -7,7 +7,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class RecipebyidPipe implements PipeTransform {
 
   transform(value: string, ...args: string[]): string {
-    return value.replace("https://api.edamam.com/api/recipes/v2/", "").split("?")[0];
+    return value.replace("https://main--u06-recipe-harveybong.netlify.app/recipe/", "").split("?")[0];
   }
 
 }
